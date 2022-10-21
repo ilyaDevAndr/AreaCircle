b00fd31a8712310cf960843da7fc4ab279bf0130
ilya1 <ilya1@DESKTOP-R4A653G>
2022-10-21 17:51:29 +0300

    Добавьте файлы проекта.

---
+++ AreaCircle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius; double area;
+            radius = 25.0;
+            area = radius * radius * 3.1416;
+            Console.WriteLine();
+            Console.WriteLine("Площадь круга " + area);
+            Console.WriteLine("Нажмите ENTER чтобы выйти из программы");
+            Console.Read();
+
+        }
+    }
+}
